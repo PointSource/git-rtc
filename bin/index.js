f92c92567775a67a75686aba39639fd074b48868
Zachary Kuhn <zachary.kuhn@gmail.com>
2015-01-08 14:23:04 -0500

    Small changes.

--- bin/index.js
+++ bin/index.js
@@ -8,6 +8,7 @@ var user = process.env.RTC_USER;
 var password = process.env.RTC_PASSWORD;
 var defaultAuthor = process.env.AUTHOR;
 var defaultDomain = process.env.DOMAIN;
+var component = 'kristameyer | springleaf Default Component';
 
 // some requests print a lot of information
 // increase the buffer to handle the size of these requests
@@ -18,35 +19,41 @@ function convertToEmail(name) {
   return [name.toLowerCase().split(/\s+/).join('.'), '@', defaultDomain].join('');
 }
 
-function processHistoryItem(history, index) {
-  if (index >= history.length) return;
+function processHistoryItem(history, index, more) {
+  if (index >= history.length) {
+    if (more && more.length) {
+      return processHistoryItem(more, 0);
+    } else {
+      return;
+    }
+  }
 
   var change = history[index],
-      author = change.author,
-      uuid = change.uuid,
-      comment = change.comment,
-      modified = new Date(change.modified).toISOString(),
-      workitem = change['workitem-label'];
-  
+    author = change.author,
+    uuid = change.uuid,
+    comment = change.comment,
+    modified = new Date(change.modified).toISOString(),
+    workitem = change['workitem-label'];
+
   // list the changes for this UUID so we can get the full work item and comment
   exec(scm + ' list changes ' + uuid + ' -u ' + user + ' -P ' + password + ' -j', {
-      maxBuffer: maxBuffer
-    }, function (err, stdout, stderr) {
+    maxBuffer: maxBuffer
+  }, function (err, stdout, stderr) {
     if (err) throw err;
 
     var jazzResponse = JSON.parse(stdout),
-        change = jazzResponse.changes[0],
-        comment = createCommitMessage(change),
-        name = (change.author || defaultAuthor),
-        email = convertToEmail(change.author || defaultAuthor),
-        // the author in the form of "John Doe <john.doe@domain>"
-        author = name + ' <' + email + '>',
-        uuid = change.uuid;
+      change = jazzResponse.changes[0],
+      comment = createCommitMessage(change),
+      name = (change.author || defaultAuthor),
+      email = convertToEmail(change.author || defaultAuthor),
+      // the author in the form of "John Doe <john.doe@domain>"
+      author = name + ' <' + email + '>',
+      uuid = change.uuid;
 
     // accept changes from RTC
     exec(scm + ' accept ' + uuid + ' -u ' + user + ' -P ' + password + ' --overwrite-uncommitted', {
-        maxBuffer: maxBuffer
-      }, function (err, stdout, stderr) {
+      maxBuffer: maxBuffer
+    }, function (err, stdout, stderr) {
       if (err) throw err;
 
       // add all changes to git
@@ -63,13 +70,14 @@ function processHistoryItem(history, index) {
           '-m "' + comment + '"',
           '--author="' + author + '"',
           '--date=' + modified,
-          '--allow-empty'].join(' '), {
+          '--allow-empty'
+        ].join(' '), {
           maxBuffer: maxBuffer
         }, function (err, stdout, stderr) {
           if (err) throw err;
 
           // process the next item
-          processHistoryItem(history, index + 1);
+          return processHistoryItem(history, index + 1, more);
         });
       });
     });
@@ -79,12 +87,13 @@ function processHistoryItem(history, index) {
 function createCommitMessage(change) {
   // convert <No comment> to an empty string.
   var comment = change.comment.replace(/<No comment>/, ''),
-      message;
+    message;
 
   if (change.workitems && change.workitems.length > 0) {
     // message is in a format similar to "12345 The work item description"
-    message = [change.workitems[0]['workitem-number'],
-      change.workitems[0]['workitem-label']].join(' ');
+    message = [change.workitems[0]['id'],
+      change.workitems[0]['workitem-label']
+    ].join(' ');
 
     // if there is a comment, append it to the message as a new paragraph
     if (comment) {
@@ -94,9 +103,12 @@ function createCommitMessage(change) {
     message = change.comment;
   }
 
+  // escape quoted parts of commit message text.
+  message = message.replace(/"/g, '\\"');
+
   return message;
 }
-
+var discardCount = 0;
 /*
   1. Grab the history of change sets from RTC.
   2. If the message has been cut off, query to RTC to get the full message.
@@ -106,14 +118,14 @@ function createCommitMessage(change) {
   6. Repeat from step 2.
  */
 function discardChanges(callback) {
-  exec(scm + ' show history -j -m 100 -u ' + user + ' -P ' + password, {
+  exec(scm + ' show history -j -m 100 -C "' + component + '" -u ' + user + ' -P ' + password, {
     maxBuffer: maxBuffer
   }, function (err, stdout, stderr) {
     if (err) throw err;
 
     // get the response and reverse all the change sets in it
     var jazzResponse = JSON.parse(stdout),
-        changes = jazzResponse.changes;
+      changes = jazzResponse.changes;
 
     // cannot discard the first change
     if (changes.length === 1) {
@@ -131,60 +143,72 @@ function discardChanges(callback) {
     }, function (err, stdout, stderr) {
       if (err) throw err;
 
+      console.log('Reversed ' + (discardCount + uuids.length) + ' change sets.');
+      discardCount = discardCount + uuids.length;
+
       // recurse and attempt to discard more changes
       discardChanges(callback);
     });
   });
 }
 
-discardChanges(walkThroughHistory);
+console.log('Rewinding History');
+
+//discardChanges(walkThroughHistory);
+walkThroughHistory();
 
 function walkThroughHistory() {
   exec('git init', function (err) {
     if (err) throw err;
 
-    exec(scm + ' show history -j -u ' + user + ' -P ' + password, {
+    exec(scm + ' show history -C "' + component + '" -j -u ' + user + ' -P ' + password, {
       maxBuffer: maxBuffer
     }, function (err, stdout, stderr) {
       if (err) throw err;
 
       var jazzResponse = JSON.parse(stdout),
-          change = jazzResponse.changes[0],
-          comment = createCommitMessage(change),
-          name = (change.author || defaultAuthor),
-          email = convertToEmail(change.author || defaultAuthor),
-          author = name + ' <' + email + '>',
-          modified = new Date(change.modified).toISOString();
+        change = jazzResponse.changes[0],
+        comment = createCommitMessage(change),
+        name = (change.author || defaultAuthor),
+        email = convertToEmail(change.author || defaultAuthor),
+        author = name + ' <' + email + '>',
+        modified = new Date(change.modified).toISOString();
 
       exec('git add -A', function (err, stdout, stderr) {
         if (err) throw err;
 
         exec(['GIT_COMMITTER_EMAIL="' + email + '"',
-            'GIT_COMMITTER_NAME="' + name + '"',
-            'GIT_COMMITTER_DATE="' + modified + '"',
-            'git commit',
-            '-m "' + comment + '"',
-            '--author="' + author + '"',
-            '--date=' + modified,
-            '--allow-empty'].join(' '), function (err, stdout, stderr) {
+          'GIT_COMMITTER_NAME="' + name + '"',
+          'GIT_COMMITTER_DATE="' + modified + '"',
+          'git commit',
+          '-m "' + comment + '"',
+          '--author="' + author + '"',
+          '--date=' + modified,
+          '--allow-empty'
+        ].join(' '), {
+          maxBuffer: maxBuffer
+        }, function (err, stdout, stderr) {
           if (err) throw err;
 
-          exec(scm + ' show status -i in:cbC -j -u ' + user + ' -P ' + password, {
-              maxBuffer: maxBuffer
-            }, function (err, stdout, stderr) {
-              if (err) throw err;
+          exec(scm + ' show status -j -u ' + user + ' -P ' + password, {
+            maxBuffer: maxBuffer
+          }, function (err, stdout, stderr) {
+            if (err) throw err;
 
-              var jazzResponse = JSON.parse(stdout);
+            var jazzResponse = JSON.parse(stdout);
 
-              // get the RTC change set history and reverse it to get it in
-              // chronological order
-              var orderedHistory = jazzResponse.workspaces[0]
-                  .components[0]['incoming-changes'].reverse();
+            // get the RTC change set history and reverse it to get it in
+            // chronological order
+            var orderedHistory = jazzResponse.workspaces[0]
+              .components[0]['incoming-changes'].reverse();
 
-              processHistoryItem(orderedHistory, 0);
-            });
+            var suspendedItems = jazzResponse.workspaces[0]
+              .components[0].suspended.reverse();
+
+            processHistoryItem(suspendedItems, 0, orderedHistory);
           });
         });
       });
+    });
   });
 }
